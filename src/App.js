6ca6910d257bef4d4055c5a7fa9302e2113652c4
yace1 <yacinbent@gmail.com>
2022-11-08 15:29:36 +0100

    added linkedin link

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import Weather from './components/Weather';
 function App() {
   return (
     <div className="App">
+		<a href="https://www.linkedin.com/in/yacine-bentayeb/" className="linkedinLink">click to Add me on Linkedin: Yacine Bentayeb</a>
 		<Weather />
+		<h3 className='mailAddress'>yacinbent@gmail.com</h3>
     </div>
   );
 }
